6d1479b4cbe8270829389ee3f11d37d8ac28c28d
IvanKornieiev <bomak54@gmail.com>
2023-03-17 11:04:45 +0200

    add  sass component Main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,8 +3,11 @@ const Main = () => {
     return (
         <main className='main'>
             <div className='main-container'>
-                <span>20% OFF DURING THE WEEKEND</span>
-                <button className='main-button'>Get Now</button>
+                <div className='main-container-text'>
+                    <span>The new phones are here take a look</span>
+                    <p>The display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle.  Actual viewable area is less.</p>
+                    <button className='main-button'>Get Now</button>
+                </div>
                 <div className='main-container-img'></div>
             </div>
         </main>
