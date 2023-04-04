8019a44296abdcb7b72e6868af5ccf90ff7fc492
IvanKornieiev <bomak54@gmail.com>
2023-04-04 14:59:59 +0300

    add category styles

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -5,16 +5,35 @@ import {useTypedSelector} from "../../Hooks/productTypeSelector";
 
 
 const Category = () => {
-    const {getCategory} = useActions();
-    const {category} = useTypedSelector(state => state.category)
-    console.log(111,category)
-    useEffect(() => {
-        getCategory()
-    }, [])
-    return (
-        <div>
+        const {getCategory} = useActions();
+        const {category} = useTypedSelector(state => state.category);
 
-        </div>
-    )
-};
+        useEffect(() => {
+            getCategory();
+        }, [])
+        return (
+            <div className='category'>
+                <h1>Category</h1>
+                <div className='category-wrap'>
+                    <div className='category-item'>
+                        <img src={require('../../img/electronics.jpeg')} alt='electronics'/>
+                        <span>{category[0]}</span>
+                    </div>
+                    <div className='category-item'>
+                        <img src={require('../../img/jewelery.jpeg')} alt='jewelery'/>
+                        <span>{category[1]}</span>
+                    </div>
+                    <div className='category-item'>
+                        <img src={require("../../img/means.jpeg")} alt='means'/>
+                        <span>{category[2]}</span>
+                    </div>
+                    <div className='category-item'>
+                        <img src={require("../../img/womens.jpeg")} alt='womens'/>
+                            <span>{category[3]}</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+;
 export default Category;
